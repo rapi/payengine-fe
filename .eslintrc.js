7ce45e9ed2d7c05a5e526f3c6b0338bb5c4c0225
Vasile Grinciuc <vasilegrinciuc@ip-10-151-1-44.eu-central-1.compute.internal>
2022-10-09 00:57:13 +0300

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
   env: {
-    browser: true, // Browser global variables like `window` etc.
-    commonjs: true, // CommonJS global variables and CommonJS scoping.Allows require, exports and module.
-    es6: true, // Enable all ECMAScript 6 features except for modules.
-    jest: true, // Jest global variables like `it` etc.
-    node: true // Defines things like process.env when generating through node
+    browser: true,
+    es2021: true,
+    jest: true
   },
-  extends: [],
-  parser: "babel-eslint", // Uses babel-eslint transforms.
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -17,7 +14,9 @@ module.exports = {
   },
   plugins: [],
   root: true, // For configuration cascading.
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off"
+  },
   settings: {
     react: {
       version: "detect" // Detect react version
